b7550009725ab56616ae793c8992294ce5d89fe1
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2024-11-25 08:04:54 +0545

    notFistCommit

---
+++ AVSH Test project/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace AVSH_Test_project
+{
+public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string EnrollmentDate { get; set; }
+        public string Address { get; set; }
+        public string CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
